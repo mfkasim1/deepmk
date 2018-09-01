59f4fdef79717f54e11066c39de1a2b812168089
mfkasim91 <firman.kasim@gmail.com>
2018-09-01 22:39:38 +0100

    Modify eval.py to use a deepmk function

--- scripts/hymenoptera/eval.py
+++ scripts/hymenoptera/eval.py
@@ -6,12 +6,15 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
 import numpy as np
-import matplotlib.pyplot as plt
 import torchvision
 from torchvision import datasets, models, transforms
+import deepmk
 
-plt.ion()
+# data directory
+fdir = os.path.split(os.path.abspath(__file__))[0]
+data_dir = os.path.join(fdir, "..", "..", "dataset", "hymenoptera_data")
 
+# transformations of the data
 data_transforms = {
     "train": transforms.Compose([
         transforms.RandomResizedCrop(224),
@@ -27,11 +30,7 @@ data_transforms = {
     ]),
 }
 
-data_dir = os.path.join("..", "..", "dataset", "hymenoptera_data")
-def print_and_return(x):
-    print(x)
-    return x
-
+# dataset where the data should be loaded
 image_datasets = {
     x: datasets.ImageFolder(root=os.path.join(data_dir, x),
                             transform=data_transforms[x])
@@ -44,75 +43,6 @@ dataloaders = {
     for x in ["train", "val"]
 }
 
-dataset_sizes = {x: len(image_datasets[x]) for x in ["train", "val"]}
-class_names = image_datasets["train"].classes
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
-    since = time.time()
-    best_model_weights = copy.deepcopy(model.state_dict())
-    best_acc = 0.0
-
-    for epoch in range(num_epochs):
-        print("Epoch %d/%d" % (epoch+1, num_epochs))
-        print("-"*10)
-
-        # every epoch has a training and a validation phase
-        for phase in ["train", "val"]:
-            if phase == "train":
-                scheduler.step() # adjust the training learning rate
-                model.train() # set the model to the training mode
-            else:
-                model.eval() # set the model to the evaluation mode
-
-            running_loss = 0.0
-            running_corrects = 0
-
-            # iterate over the data
-            for inputs, labels in dataloaders[phase]:
-                # load the inputs and the labels to the working device
-                inputs = inputs.to(device)
-                labels = labels.to(device)
-
-                # reset the model gradient to 0
-                optimizer.zero_grad()
-
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == "train"):
-                    outputs = model(inputs)
-                    _, preds = torch.max(outputs, 1)
-                    loss = criterion(outputs, labels)
-
-                    # backward gradient computation and optimize in training
-                    if phase == "train":
-                        loss.backward()
-                        optimizer.step()
-
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
-
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects.double() / dataset_sizes[phase]
-
-            print("%s loss: %.4f, acc: %.4f" % (phase, epoch_loss, epoch_acc))
-
-            # copy the best model
-            if phase == "val" and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_weights = copy.deepcopy(model.state_dict())
-        print("")
-
-    time_elapsed = time.time()- since
-    print("Training complete in %fs" % time_elapsed)
-    print("Best val acc: %.4f" % best_acc)
-
-    # load the best model
-    model.load_state_dict(best_model_weights)
-    return model
-
 # load the pretrained resnet18
 model_ft = models.resnet18(pretrained=True)
 for param in model_ft.parameters():
@@ -122,9 +52,7 @@ for param in model_ft.parameters():
 num_ftrs = model_ft.fc.in_features
 model_ft.fc = nn.Linear(num_ftrs, 2)
 
-# load the model to the device
-model_ft = model_ft.to(device)
-
+# cross entropy loss for 2 classes classifications
 criterion = nn.CrossEntropyLoss()
 
 optimizer_ft = optim.SGD(model_ft.fc.parameters(), lr=0.001, momentum=0.9)
@@ -132,5 +60,6 @@ optimizer_ft = optim.SGD(model_ft.fc.parameters(), lr=0.001, momentum=0.9)
 # reduce the learning rate by 0.1 every 7 epochs
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
-model_ft = train_model(model_ft, criterion, optimizer_ft,
-    exp_lr_scheduler, num_epochs=25)
+# train the model
+model_ft = deepmk.spv.train(model_ft, dataloaders, criterion, optimizer_ft,
+    scheduler=exp_lr_scheduler, num_epochs=25)
