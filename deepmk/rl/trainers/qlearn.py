3740de55af6801f1039c9bdade545a54a122673a
mfkasim91 <firman.kasim@gmail.com>
2018-09-09 21:52:53 +0100

    Update_every QLearn

--- deepmk/rl/trainers/qlearn.py
+++ deepmk/rl/trainers/qlearn.py
@@ -1,3 +1,4 @@
+import copy
 from collections import deque
 from torch.utils.data import DataLoader
 from deepmk.rl.trainers.trainer import Trainer
@@ -6,13 +7,17 @@ from deepmk.rl.dataloaders import LastStepLoader
 __all__ = ["QLearn"]
 
 class QLearn(Trainer):
-    def __init__(self, qnet, optimizer, gamma=0.9,
+    def __init__(self, qnet, optimizer, gamma=0.9, update_every=1,
                  rldataloader=LastStepLoader()):
         self.qnet = qnet
         self.optimizer = optimizer
         self.tuples = []
         self.gamma = gamma
+        self.update_every = update_every
         self.rldataloader = rldataloader
+
+        self.qnet_old = copy.deepcopy(qnet)
+        self.num_steps = 0
 
     def trainstep(self, state, action, reward, next_state, done):
         # save the episode tuple
@@ -26,7 +31,7 @@ class QLearn(Trainer):
         if dataloader is not None:
             for s, a, r, snext, ep_done in dataloader:
                 # calculate the loss based on the tuple
-                snext_val = self.qnet.max_value(snext) * (1.0 - ep_done.float())
+                snext_val = self.qnet_old.max_value(snext) * (1.0 - ep_done.float())
                 train_target = r.float() + self.gamma * snext_val
                 pred_target = self.qnet.value(s, a)
 
@@ -36,3 +41,10 @@ class QLearn(Trainer):
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
+
+        # update the num steps
+        self.num_steps += 1
+        # update the fixed models
+        if self.num_steps % self.update_every == 0:
+            self.qnet_old = copy.deepcopy(self.qnet)
+            self.num_steps = 0
