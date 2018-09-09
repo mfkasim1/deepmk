cd995cd4300f3f5e46f2a509042f7be55af19bdf
mfkasim91 <firman.kasim@gmail.com>
2018-09-09 17:44:09 +0100

    Random playback memory

--- deepmk/rl/trainers/qlearn.py
+++ deepmk/rl/trainers/qlearn.py
@@ -4,19 +4,23 @@ from deepmk.rl.trainers.trainer import Trainer
 __all__ = ["QLearn", "DoubleQLearn"]
 
 class QLearn(Trainer):
-    def __init__(self, qnet, optimizer, gamma=0.9):
+    def __init__(self, qnet, optimizer, gamma=0.9, max_memory=1,
+                 dataloader_kwargs={}):
         self.qnet = qnet
         self.optimizer = optimizer
         self.tuples = []
         self.gamma = gamma
+        self.max_memory = max_memory
+        self.dataloader_kwargs = dataloader_kwargs
 
     def trainstep(self, state, action, reward, next_state, done):
         # save the episode tuple
         tup = (state, action, reward, next_state, done)
         self.tuples.append(tup)
+        if len(self.tuples) > self.max_memory: self.tuples.pop(0)
 
         # set the dataloader
-        dataloader = self.getdataloader()
+        dataloader = DataLoader(self.tuples, **self.dataloader_kwargs)
 
         # present the state and the target for the training
         if dataloader is not None:
@@ -34,26 +38,25 @@ class QLearn(Trainer):
                 loss.backward()
                 self.optimizer.step()
 
-    def getdataloader(self):
-        tup = self.tuples[-1]
-        dataloader = DataLoader([tup], batch_size=1)
-        return dataloader
-
 class DoubleQLearn(Trainer):
-    def __init__(self, qnet1, qnet2, optimizer, gamma=0.9):
+    def __init__(self, qnet1, qnet2, optimizer, gamma=0.9, max_memory=1,
+                 dataloader_kwargs={}):
         self.qnet1 = qnet1
         self.qnet2 = qnet2
         self.optimizer = optimizer
         self.tuples = []
         self.gamma = gamma
+        self.max_memory = max_memory
+        self.dataloader_kwargs = dataloader_kwargs
 
     def trainstep(self, state, action, reward, next_state, done):
         # save the episode tuple
         tup = (state, action, reward, next_state, done)
         self.tuples.append(tup)
+        if len(self.tuples) > self.max_memory: self.tuples.pop(0)
 
         # set the dataloader
-        dataloader = self.getdataloader()
+        dataloader = DataLoader(self.tuples, **self.dataloader_kwargs)
 
         if dataloader is not None:
             for (s, a, r, snext, ep_done) in dataloader:
@@ -79,8 +82,3 @@ class DoubleQLearn(Trainer):
                 loss_a.backward()
                 loss_b.backward()
                 self.optimizer.step()
-
-    def getdataloader(self):
-        tup = self.tuples[-1]
-        dataloader = DataLoader([tup], batch_size=1)
-        return dataloader
